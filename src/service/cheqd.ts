0890b5d515072e2fcf60d3d3a40df1090ac21d84
DaevMithran <daevmithran1999@gmail.com>
2022-10-18 18:12:39 +0530

    chore: Update express validators
    Signed-off-by: DaevMithran <daevmithran1999@gmail.com>

--- src/service/cheqd.ts
+++ src/service/cheqd.ts
@@ -82,9 +82,6 @@ export class CheqdRegistrar {
 }
 
 export async function CheqdResolver(id: string) {
-    if(!id.startsWith('did')) {
-        return null
-    }
     const result = await fetch(`${DefaultResolverUrl.Cheqd}/${id}`)
     if (!result.ok) { 
         return null

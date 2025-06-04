74cd96b7c425a1c528fb663c9be868cec6c6f05f
DaevMithran <61043607+DaevMithran@users.noreply.github.com>
2025-06-04 19:10:16 +0530

    fix: Allow setting own RPC URLs (#434)
    * feat: Allow overriding rpc urls
    
    * Bump devdeps
    
    * Update README.md
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Ankur Banerjee <ankurdotb@users.noreply.github.com>

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -3,6 +3,8 @@ declare global {
 		interface ProcessEnv {
 			FEE_PAYER_TESTNET_MNEMONIC: string;
 			FEE_PAYER_MAINNET_MNEMONIC: string;
+			TESTNET_RPC_URL?: string;
+			MAINNET_RPC_URL?: string;
 			LOCAL_STORE_TTL: string;
 			PORT: string;
 		}
